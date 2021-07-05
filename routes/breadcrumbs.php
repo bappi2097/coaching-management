58e0352dfda1faba78e84c1183908510c45fafa1
unknown <Vue Tork>
2021-07-06 00:04:00 +0600

    breadcrumb and pagination added

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,40 @@
+<?php
+
+/* -------------------------------------------------------------------------- */
+/*                           Admin Breadcrumb Start                           */
+/* -------------------------------------------------------------------------- */
+
+// // Dashboard
+// Breadcrumbs::for('admin.dashboard', function ($trail) {
+//     $trail->push('Dashboard', route('admin.dashboard'));
+// });
+
+// // Profile
+// Breadcrumbs::for('admin.profile', function ($trail) {
+//     $trail->push('Profile', route('admin.profile.edit'));
+// });
+
+// // Dashboard > Admin
+// Breadcrumbs::for('admin.admin', function ($trail) {
+//     $trail->parent('admin.dashboard');
+//     $trail->push('Admin', route('admin.users.admin.index'));
+// });
+
+// // Dashboard > Admin > Create
+// Breadcrumbs::for('admin.admin.create', function ($trail) {
+//     $trail->parent('admin.dashboard');
+//     $trail->push('Create', route('admin.users.admin.create'));
+// });
+
+// // Dashboard > Admin > Show
+// Breadcrumbs::for('admin.admin.show', function ($trail, $id) {
+//     $trail->parent('admin.dashboard');
+//     $trail->push('Show', route('admin.users.admin.show', $id));
+// });
+
+
+// // Dashboard > Admin > Edit
+// Breadcrumbs::for('admin.admin.edit', function ($trail, $id) {
+//     $trail->parent('admin.dashboard');
+//     $trail->push('Edit', route('admin.users.admin.edit', $id));
+// });
